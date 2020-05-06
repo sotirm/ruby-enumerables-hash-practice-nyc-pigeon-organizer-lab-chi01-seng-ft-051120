54d465b109f92f75273c996406060045d13aa2a8
Michael Sotir <sotirm@umich.edu>
2020-05-06 19:46:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,8 +4,11 @@ def nyc_pigeon_organizer(data)
   name_hash = {}
   data.each do |pigeon_attribute, attribute_hash|
     attribute_hash.each do |sub_attribute, pigeon_name_array|
-       pigeon_name_array.each
-
+       pigeon_name_array.each do |pigeon_name|
+         name_hash[pigeon_name] = {}
+  
+       end
     end
   end
+  binding.pry
 end
