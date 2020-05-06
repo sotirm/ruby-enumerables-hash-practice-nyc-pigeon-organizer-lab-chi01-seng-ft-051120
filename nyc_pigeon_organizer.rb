0020cbc6aedca536942dab2e5b70c903196477f1
Michael Sotir <sotirm@umich.edu>
2020-05-06 20:18:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  name_hash = {}
+  data.each do |pigeon_attribute, attribute_hash|
+    attribute_hash.each do |sub_attribute, pigeon_name_array|
+       pigeon_name_array.each do |pigeon_name|
+         name_hash[pigeon_name] = {:color => [], :gender => [], :lives => []}
+  
+       end
+    end
+  end
+  
+  
+  name_hash.each do |pigeon_name, pigeon_characteristics| 
+    data.each do |pigeon_attribute, attribute_hash|
+      attribute_hash.each do |sub_attribute, pigeon_name_array|
+        if (pigeon_name_array.any?(pigeon_name))
+          name_hash[pigeon_name][pigeon_attribute] << sub_attribute.to_s
+        end
+      end
+    end
+  end
+  name_hash
 end
+
+
+
+
+
